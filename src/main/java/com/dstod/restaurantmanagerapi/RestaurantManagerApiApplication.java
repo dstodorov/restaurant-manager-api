59008cb39d9de7d2df5b0aee7e4bf8a842e95171
Dimitar Todorov <d.todorov.vn@gmail.com>
2023-10-23 16:38:05 +0300

    Initial project

---
+++ src/main/java/com/dstod/restaurantmanagerapi/RestaurantManagerApiApplication.java
@@ -0,0 +1,13 @@
+package com.dstod.restaurantmanagerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantManagerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantManagerApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dstod/restaurantmanagerapi/RestaurantManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dstod.restaurantmanagerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantManagerApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
