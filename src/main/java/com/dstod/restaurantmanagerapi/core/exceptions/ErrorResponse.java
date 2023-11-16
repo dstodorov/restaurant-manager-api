b419ad311569a2bc1f30be98397076727d55deb8
Dimitar Todorov <d.todorov.vn@gmail.com>
2023-11-16 14:36:01 +0200

    Convert ErrorRespone from class to record

--- src/main/java/com/dstod/restaurantmanagerapi/core/exceptions/ErrorResponse.java
+++ src/main/java/com/dstod/restaurantmanagerapi/core/exceptions/ErrorResponse.java
@@ -1,19 +1,4 @@
 package com.dstod.restaurantmanagerapi.core.exceptions;
 
-public class ErrorResponse {
-    private final String module;
-    private final String exceptionMessage;
-
-    public ErrorResponse(String module, String exceptionMessage) {
-        this.module = module;
-        this.exceptionMessage = exceptionMessage;
-    }
-
-    public String getModule() {
-        return module;
-    }
-
-    public String getExceptionMessage() {
-        return exceptionMessage;
-    }
+public record ErrorResponse(String module, String exceptionMessage) {
 }
