2fc0026be64929d29724cd2a1b6878ff0b1f946e
Dimitar Todorov <d.todorov.vn@gmail.com>
2023-12-06 15:53:17 +0200

    Add bearer authorization in swagger-ui

--- src/main/java/com/dstod/restaurantmanagerapi/common/config/OpenAPIConfig.java
+++ src/main/java/com/dstod/restaurantmanagerapi/common/config/OpenAPIConfig.java
@@ -1,24 +1,42 @@
 package com.dstod.restaurantmanagerapi.common.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@OpenAPIDefinition
 public class OpenAPIConfig {
+
     @Bean
     public OpenAPI openAPI() {
+        final String securitySchemeName = "bearerAuth";
         return new OpenAPI().info(
-                new Info()
-                        .title("Restaurant Manager API")
-                        .contact(new Contact()
-                                .name("Dimitar Todorov")
-                                .email("d.todorov.vn@gmail.com")
-                        )
-                        .description("This API is made to be consumed by Web and Mobile front-end applications")
-                        .version("v0.1")
-        );
+                        new Info()
+                                .title("Restaurant Manager API")
+                                .contact(new Contact()
+                                        .name("Dimitar Todorov")
+                                        .email("d.todorov.vn@gmail.com")
+                                )
+                                .description("This API is made to be consumed by Web and Mobile front-end applications")
+                                .version("v0.1")
+                )
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(
+                        new Components()
+                                .addSecuritySchemes(securitySchemeName,
+                                        new SecurityScheme()
+                                                .name(securitySchemeName)
+                                                .type(SecurityScheme.Type.HTTP)
+                                                .scheme("bearer")
+                                                .bearerFormat("JWT")
+                                )
+                );
     }
 }
