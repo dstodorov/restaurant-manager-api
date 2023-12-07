a807a85318f026307b4766353043d64a56211a04
Dimitar Todorov <d.todorov.vn@gmail.com>
2023-12-07 16:10:56 +0200

    Refactor openAPI configuration to uses values from application.yml file

--- src/main/java/com/dstod/restaurantmanagerapi/common/config/OpenAPIConfig.java
+++ src/main/java/com/dstod/restaurantmanagerapi/common/config/OpenAPIConfig.java
@@ -1,31 +1,50 @@
 package com.dstod.restaurantmanagerapi.common.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@OpenAPIDefinition
+@OpenAPIDefinition(servers = @Server(url = "https://codesharing.org/restaurant"))
 public class OpenAPIConfig {
+
+    @Value("${api.info.security.scheme.name}")
+    private String securitySchemeNameProperty;
+    @Value("${api.info.security.scheme.type}")
+    private String securitySchemeTypeProperty;
+    @Value("${api.info.security.scheme.format}")
+    private String securitySchemeFormatProperty;
+    @Value("${api.info.title}")
+    private String apiInfoTitleProperty;
+    @Value("${api.info.contact.name}")
+    private String apiInfoContactNameProperty;
+    @Value("${api.info.contact.email}")
+    private String apiInfoContactEmailProperty;
+    @Value("${api.info.description}")
+    private String apiInfoDescriptionProperty;
+    @Value("${api.info.version}")
+    private String apiInfoVersionProperty;
 
     @Bean
     public OpenAPI openAPI() {
-        final String securitySchemeName = "bearerAuth";
+        final String securitySchemeName = securitySchemeNameProperty;
         return new OpenAPI().info(
                         new Info()
-                                .title("Restaurant Manager API")
+                                .title(apiInfoTitleProperty)
                                 .contact(new Contact()
-                                        .name("Dimitar Todorov")
-                                        .email("d.todorov.vn@gmail.com")
+                                        .name(apiInfoContactNameProperty)
+                                        .email(apiInfoContactEmailProperty)
                                 )
-                                .description("This API is made to be consumed by Web and Mobile front-end applications")
-                                .version("v0.1")
+                                .description(apiInfoDescriptionProperty)
+                                .version(apiInfoVersionProperty)
                 )
                 .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
                 .components(
@@ -34,8 +53,8 @@ public class OpenAPIConfig {
                                         new SecurityScheme()
                                                 .name(securitySchemeName)
                                                 .type(SecurityScheme.Type.HTTP)
-                                                .scheme("bearer")
-                                                .bearerFormat("JWT")
+                                                .scheme(securitySchemeTypeProperty)
+                                                .bearerFormat(securitySchemeFormatProperty)
                                 )
                 );
     }
