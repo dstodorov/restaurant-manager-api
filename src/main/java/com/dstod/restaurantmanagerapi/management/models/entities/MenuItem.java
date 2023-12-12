497713203c564e7873ba091577249ef4dcffec54
Dimitar Todorov <d.todorov.vn@gmail.com>
2023-12-12 10:45:10 +0200

    Add product_id to the menu item and make menu_id NOT NULL

--- src/main/java/com/dstod/restaurantmanagerapi/management/models/entities/MenuItem.java
+++ src/main/java/com/dstod/restaurantmanagerapi/management/models/entities/MenuItem.java
@@ -1,5 +1,6 @@
 package com.dstod.restaurantmanagerapi.management.models.entities;
 
+import com.dstod.restaurantmanagerapi.inventory.models.entities.Product;
 import com.dstod.restaurantmanagerapi.inventory.models.entities.Recipe;
 import jakarta.persistence.*;
 
@@ -15,22 +16,30 @@ public class MenuItem {
     @Column(nullable = false)
     private BigDecimal price;
 
+    @Column(nullable = false)
+    private String additionalInformation;
+
     @ManyToOne
-    @JoinColumn(name = "menu_id")
+    @JoinColumn(name = "menu_id", nullable = false)
     private Menu menu;
 
     @OneToOne
     @JoinColumn(name = "recipe_id")
     private Recipe recipe;
 
+    @OneToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
     public MenuItem() {
     }
 
-    public MenuItem(long id, BigDecimal price, Menu menu, Recipe recipe) {
+    public MenuItem(long id, BigDecimal price, Menu menu, Recipe recipe, Product product) {
         this.id = id;
         this.price = price;
         this.menu = menu;
         this.recipe = recipe;
+        this.product = product;
     }
 
     public long getId() {
@@ -66,6 +75,15 @@ public class MenuItem {
 
     public MenuItem setRecipe(Recipe recipe) {
         this.recipe = recipe;
+        return this;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public MenuItem setProduct(Product product) {
+        this.product = product;
         return this;
     }
 }
